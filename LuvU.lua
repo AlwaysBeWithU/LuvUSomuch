128e65b017c23147390716388ffb3b5a852b4ae0
AlwaysBeWithU <thabthai00@gmail.com>
2026-05-07 10:51:39 +0700

    Update LuvU.lua

--- LuvU.lua
+++ LuvU.lua
@@ -1,20 +1,29 @@
 repeat
     task.wait(0.1)
-until game:IsLoaded() and _G.Horst_SetDescription 
+until game:IsLoaded() and _G.Horst_SetDescription
 
-local Players = game:GetService("Players")
-local plr = Players.LocalPlayer
-local stats = plr:WaitForChild("leaderstats") 
+local plr = game.Players.LocalPlayer
+local currencies = plr.PlayerGui:WaitForChild("Interface")
+    :WaitForChild("Topbar")
+    :WaitForChild("Main")
+    :WaitForChild("Currencies")
 
-local function updateStatus()
-    local gold = stats:FindFirstChild("Gold") and stats.Gold.Value or 0
-    local gem = stats:FindFirstChild("Gems") and stats.Gems.Value or 0
-    local lv = stats:FindFirstChild("Level") and stats.Level.Value or 0
-    
-    local prestige = stats:FindFirstChild("Prestige") and stats.Prestige.Value or 0
+local titleLabel = plr.PlayerGui.Interface.Equipment.Prestige.Progress.Title
 
-    local messages = "LV : " .. tostring(lv) .. " | ⚔️ Prestige : " .. tostring(prestige) .. " | 💰 : " .. tostring(gold) .. " | 💎 : " .. tostring(gem)
- 
-    
+local prestigeLevel = {
+    ["CADET"] = 0,
+    ["PRIVATE"] = 1,
+    ["CORPORAL"] = 2,
+    ["SERGEANT"] = 3,
+    ["COMMANDER"] = 4,
+    ["MASTER SERGEANT"] = 5,
+}
+
+while task.wait(5) do
+    local gold = currencies.Gold.Amount.Text
+    local gem  = currencies.Gems.Amount.Text
+    local prestige = prestigeLevel[titleLabel.Text] or 0
+
+    local messages = "💰 Gold : " .. gold .. " , 💎 Gems : " .. gem .. " , ⚔️ Prestige : " .. prestige
     _G.Horst_SetDescription(messages)
 end
